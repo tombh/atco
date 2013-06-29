57d836e52bcdc61d918e47b6ca5838887c9881b9
Thomas Buckley-Houston <tom@tombh.co.uk>
2013-06-29 20:55:40 +0100

    Added arrival time

--- lib/atco/stop.rb
+++ lib/atco/stop.rb
@@ -2,7 +2,13 @@ module Atco
 
   class Stop
 
-    attr_accessor :bay_number, :location, :timing_point_indicator, :fare_stage_indicator, :published_departure_time, :record_identity
+    attr_accessor :bay_number, 
+                  :location, 
+                  :timing_point_indicator, 
+                  :fare_stage_indicator, 
+                  :published_arrival_time,
+                  :published_departure_time,
+                  :record_identity
 
     def origin?; @record_identity == "QO"; end
     def intermediate?; @record_identity == "QI"; end
@@ -13,6 +19,7 @@ module Atco
       @location = data[:location]
       @timing_point_indicator = data[:timing_point_indicator]
       @fare_stage_indicator = data[:fare_stage_indicator]
+      @published_arrival_time = data[:published_arrival_time]
       @published_departure_time = data[:published_departure_time]
       @record_identity = data[:record_identity]
     end
@@ -21,6 +28,7 @@ module Atco
       {
         :record_identity => @record_identity,
         :location => @location,
+        :published_arrival_time => @published_arrival_time,
         :published_departure_time => @published_departure_time,
         :timing_point_indicator => @timing_point_indicator,
         :fare_stage_indicator => @fare_stage_indicator,
